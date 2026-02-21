b80f68c804aa17ea4d2573039120cd392318859e
Roux Kazi <kazi.rousseau@gmail.com>
2026-02-21 04:15:58 +0000

    deploy v2: AI chat, weekly view, life events

---
+++ _next/static/la3UVnXkPbH5s7wHgV885/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/wellness-coach//_next/:path+",
+        "destination": "/wellness-coach/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
